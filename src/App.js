feefd7582a204759be02be1c836b86a71458251c
kaizendevar <pmartinacosta@gmail.com>
2020-11-07 01:54:59 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Create from "./components/Create";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Inicio</Link>
+            </li>
+            <li>
+              <Link to="/create">Crear</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/create" component={Create} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Create.js
@@ -0,0 +1,21 @@
+export default function Create() {
+  return (
+    <div style={{ padding: 20 }}>
+      <div>Completa tus datos</div>
+      <div
+        style={{
+          display: "flex",
+          padding: 20,
+          border: "2px solid red",
+          justifyContent: "space-around",
+        }}
+      >
+        <div>Opcion 1</div>
+        <div>Opcion 2</div>
+        <div>Opcion 3</div>
+        <div>Opcion 4</div>
+        <div>Opcion 5</div>
+      </div>
+    </div>
+  );
+}
